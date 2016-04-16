bc87dec730f15a44b1faeb454445af2e76ac9638
Gabriel Amboss <gabriel.amboss@gmail.com>
2016-04-15 23:36:03 -0300

    Shapershifter script added

---
+++ Shapeshift/Assets/Resources/Scripts/ShapeShifter.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShapeShifter : MultiplayerBehaviour {
+
+	enum element{rock, paper, scissors};
+	[SerializeField]
+	element currElement = element.rock;
+
+	private element GetNext(element element){
+		switch(element)
+			{
+				case element.paper:
+					return element.scissors;
+				case element.scissors:
+					return element.rock;
+				case element.rock:
+					return element.paper;
+			}
+			return element.rock; //Se não receber um dos 3, o default é rock
+	}
+
+	void Update(){
+		if(Input.GetButton(PlayerID + "Shapeshift")){
+			currElement = GetNext(currElement);
+		}
+	}
+}
