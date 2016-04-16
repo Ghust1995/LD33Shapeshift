196fcf82b369938dca97df7582c2cdfc33128aba
Gustavo Guimaraes <gustavo.ceci95@gmail.com>
2016-04-15 23:15:13 -0300

    First Commit - Basic Stuff

---
+++ Shapeshift/Assets/Resources/Scripts/MultiplayerBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class MultiplayerBehaviour : MonoBehaviour {
+
+    protected int PlayerID;
+
+    void Start()
+    {
+        PlayerID = int.Parse(gameObject.name.Remove(0, gameObject.name.Length));
+    }
+
+    protected string AxisString(string axisName)
+    {
+        return PlayerID + "_" + axisName;
+    }
+}
---
+++ Shapeshift/Assets/Resources/Scripts/TopDownMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TopDownMovement : MultiplayerBehaviour {
+
+    Rigidbody2D rigidbody;
+
+    [SerializeField]
+    float MOVE_SPEED;
+    [SerializeField]
+    float TURN_SPEED;
+
+    // Use this for initialization
+    void Start () {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        rigidbody.velocity = MOVE_SPEED * Input.GetAxis(AxisString("Vertical")) * transform.up;
+        Debug.Log(rigidbody.velocity);
+        transform.Rotate(0,0,-TURN_SPEED * Input.GetAxis(AxisString("Horizontal")));
+    }
+}
